4648430b2fb6a1f0ba77fe66e44d09495b33d454
Anton Myzenkov <amyzenkov@gmail.com>
2016-06-25 22:41:38 +0300

    change style

--- test.cpp
+++ test.cpp
@@ -1,57 +1,58 @@
 #include <gtkmm.h>
 #include <iostream>
 
+using std::cerr;
+using std::endl;
+
 Gtk::Dialog* pDialog = nullptr;
 
-static
-void on_button_clicked()
+static void on_button_clicked()
 {
-  if(pDialog)
-    pDialog->hide(); //hide() will cause main::run() to end.
+	if(pDialog)
+	{
+		pDialog->hide();
+	}
 }
 
-int main (int argc, char **argv)
+int main(int argc, char *argv[])
 {
-  auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-  //Load the GtkBuilder file and instantiate its widgets:
-  auto refBuilder = Gtk::Builder::create();
-  try
-  {
-    refBuilder->add_from_file("test.glade");
-  }
-  catch(const Glib::FileError& ex)
-  {
-    std::cerr << "FileError: " << ex.what() << std::endl;
-    return 1;
-  }
-  catch(const Glib::MarkupError& ex)
-  {
-    std::cerr << "MarkupError: " << ex.what() << std::endl;
-    return 1;
-  }
-  catch(const Gtk::BuilderError& ex)
-  {
-    std::cerr << "BuilderError: " << ex.what() << std::endl;
-    return 1;
-  }
+	auto refBuilder = Gtk::Builder::create();
+	try
+	{
+		refBuilder->add_from_file("test.glade");
+	}
+	catch(const Glib::FileError& ex)
+	{
+		cerr << "FileError: " << ex.what() << endl;
+		return 1;
+	}
+	catch(const Glib::MarkupError& ex)
+	{
+		cerr << "MarkupError: " << ex.what() << endl;
+		return 1;
+	}
+	catch(const Gtk::BuilderError& ex)
+	{
+		cerr << "BuilderError: " << ex.what() << endl;
+		return 1;
+	}
 
-  //Get the GtkBuilder-instantiated Dialog:
-  refBuilder->get_widget("DialogBasic", pDialog);
-  if(pDialog)
-  {
-    //Get the GtkBuilder-instantiated Button, and connect a signal handler:
-    Gtk::Button* pButton = nullptr;
-    refBuilder->get_widget("quit_button", pButton);
-    if(pButton)
-    {
-      pButton->signal_clicked().connect( sigc::ptr_fun(on_button_clicked) );
-    }
+	refBuilder->get_widget("DialogBasic", pDialog);
+	if (pDialog)
+	{
+		Gtk::Button* pButton = nullptr;
+		refBuilder->get_widget("quit_button", pButton);
+		if (pButton)
+		{
+			pButton->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+		}
 
-    app->run(*pDialog);
-  }
+		app->run(*pDialog);
+	}
 
-  delete pDialog;
+	delete pDialog;
 
-  return 0;
+	return 0;
 }
